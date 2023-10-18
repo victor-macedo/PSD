80b83f84e19061f8431520dfc63d08d347e85f8e
larii4272 <larissa.montefusco7@gmail.com>
2023-10-18 17:45:04 +0100

    FINAL - AULA 18.10

--- Project_3/ControlUnit.vhd
+++ Project_3/ControlUnit.vhd
@@ -6,7 +6,7 @@ entity ControlUnit is
     Port (
         clk, rst_control : in std_logic;
         enable:  out std_logic_vector(3 downto 0);
-        done_1: out std_logic;
+        en_w, en_p: out std_logic;
         rst_dpath : out std_logic
     );
 end ControlUnit;
@@ -16,6 +16,7 @@ architecture Behavioral of ControlUnit is
 type fsm_states is (s_initial, s_end, CYCLE1, CYCLE2, CYCLE3);
     
     signal currstate, nextstate: fsm_states;
+    signal counter_31: unsigned (4 downto 0);
     begin
         
         state_reg: process (clk, rst_control)
@@ -30,29 +31,38 @@ type fsm_states is (s_initial, s_end, CYCLE1, CYCLE2, CYCLE3);
             end if ;
         end process;
         
+        --VER OS ENABLES!!
         state_comb: process (currstate)
         begin
             rst_dpath <= rst_control;
             nextstate <= currstate ; 
             case currstate is
                 when s_initial =>
+                     if counter_31(2 downto 0)="111" then 
+                           en_w <= '1';
+                           if counter_31(4 downto 0) = "11111" then
+                            en_p <= '1';
+                            end if;
+                     else
+                         en_w <= '0';
+                         en_p <= '0';
+                     end if;     
                      nextstate <= CYCLE1;
                      enable <= "0000";
+                                 
+                     
                 when CYCLE1 =>
                      nextstate <= CYCLE2;
                      enable <= "0001"; --MSB = Reg2, LSB = Reg1, AND = Reg3
-                     done_1 <= '0';
                 when CYCLE2 =>
                      nextstate <= CYCLE3;
                      enable <= "0010";
-                     done_1 <= '0';
                 when CYCLE3 =>
                      nextstate <= s_end;
                      enable <= "0100";
-                     done_1 <= '0';
                 when s_end =>
                      enable <= "0000";
-                     done_1 <= '1';
+                     counter_31 <= counter_31 + 1;
                    
                     
              end case;
