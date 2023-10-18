0bc9548d331fac2ffc0f0085a44821e9c8a4990b
Victor S Macedo <victor_macedo@usp.br>
2023-10-18 17:46:13 +0100

    fim aula 18_10

--- Project_3/Datapath.vhd
+++ Project_3/Datapath.vhd
@@ -4,82 +4,109 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Datapath is
   Port ( 
-    enable : in std_logic_vector (3 downto 0); --Conferir n? de registros necess?rios
-    clk, rst_dpath, done_1 : in std_logic;
-    p : in std_logic_vector (1023 downto 0);
-    w1 : in std_logic_vector (5119 downto 0);
-    res: out std_logic_vector (15 downto 0)
+    enable : in std_logic_vector (1 downto 0); --Conferir n° de registros necessários
+    clk, reset, done : in std_logic;
+    feito : out std_logic;
+    selector: in std_logic; --Conferir n° de estados da control unit
+    p1_reg : in std_logic_vector (7 downto 0); -- Tamanho de 0 a +256
+    p2_reg : in std_logic_vector (7 downto 0); -- Tamanho de 0 a +256
+    p3_reg : in std_logic_vector (7 downto 0); -- Tamanho de 0 a +256
+    p4_reg : in std_logic_vector (7 downto 0); -- Tamanho de 0 a +256
+    w1_reg: in std_logic_vector (7 downto 0); -- Tamanho de -128 a 127
+    w2_reg: in std_logic_vector (7 downto 0); -- Tamanho de -128 a 127
+    w3_reg: in std_logic_vector (7 downto 0); -- Tamanho de -128 a 127
+    w4_reg: in std_logic_vector (7 downto 0); -- Tamanho de -128 a 127
+    res : out std_logic_vector(18 downto 0)  --Resultado 
     );
 end Datapath;
 
 architecture Behavioral of Datapath is
 
-signal mul_1, mul_2, mul_3, mul_4 : signed (4 downto 0);  --Multiplicação de 1bit por 4bits
-signal res_add_sg1, res_add_sg2, res_add1, res_add2 : signed(5 downto 0);
-signal res_add_sg4 : signed(15 downto 0);
-signal res_add_sg3 : signed(6 downto 0);
-signal reg_add_sg3 : signed(6 downto 0);
-signal accum: signed(15 downto 0):=( others => '0'); -- somatorio de 1024 (2**10) de numeros signed 4 bits
-signal en_r1, en_r2, en_r3, en_r4: std_logic;
+signal p_sel1, p_sel2  :std_logic_vector (7 downto 0);
+signal p_sg1, p_sg2  :signed(8 downto 0);
+signal w_sel1, w_sel2  :std_logic_vector(7 downto 0);
+signal w_sg1, w_sg2 : signed (7 downto 0);
+signal mul_sg1, mul_sg2: signed(16 downto 0);
+signal res_mul1, res_mul2 : std_logic_vector(16 downto 0);
+
+signal res_add_sg1 : signed(17 downto 0);
+signal res_add_sg2 : signed(18 downto 0);
+signal res_add1,reg1 : std_logic_vector(17 downto 0);
+signal reg2 : std_logic_vector(18 downto 0);
+signal res_add2 : std_logic_vector(18 downto 0);
+signal en_r1, en_r2: std_logic;
 signal reg1_sg : signed (17 downto 0);
-signal p_reg : std_logic_vector (1023 downto 0):= (p);
-signal w1_reg : std_logic_vector(5119 downto 0):=(w1);
 
 begin
- en_r4 <= enable(3);
- en_r3 <= enable(2);
  en_r2 <= enable(1);
  en_r1 <= enable(0);
 
---Mux1 entrada Por se tratar de uma multiplicação de 0 e 1 um mux é mais adequado
+--Mux1 entrada
     process (clk)
         begin
-            case p(0) is
-                 when '0' => mul_1 <= "00000";
-                 when others =>  mul_1 <= signed(w1(4 downto 0));
+            case selector is
+                 when '0' => p_sel1 <= p4_reg;
+                             w_sel1 <=w4_reg; 
+                 when others =>  p_sel1 <= p2_reg;
+                                 w_sel1 <=w2_reg; 
             end case;   
-            case p(1) is
-                 when '0' => mul_2 <= "00000";
-                 when others =>  mul_2 <= signed(w1(9 downto 5));
-            end case;
-            case p(2) is
-                 when '0' => mul_3 <= "00000";
-                 when others =>  mul_3 <= signed(w1(14 downto 10));
-            end case;
-            case p(3) is
-                 when '0' => mul_4 <= "00000";
-                 when others =>  mul_4 <= signed(w1(19 downto 15));
-            end case;
         end process;
         
+ --Mux2 entrada
+    process (clk)
+        begin
+            case selector is
+                 when '0' => p_sel2 <= p1_reg;
+                             w_sel2 <=w1_reg; 
+                 when others =>  p_sel2 <= p3_reg;
+                                 w_sel2 <=w3_reg; 
+            end case;   
+        end process;
+-- Multiplier  1
+    p_sg1 <= '0' & signed(p_sel1);
+    w_sg1 <= signed(w_sel1); 
+    mul_sg1 <= p_sg1 * w_sg1;           --Se houver erro concatenar 0 no w
+    --res_mul1 <= std_logic_vector(mul_sg1);
+    
+-- Multiplier 2 
+    p_sg2 <= '0' & signed(p_sel2);
+    w_sg2 <= signed(w_sel2); 
+    mul_sg2 <= p_sg2 * w_sg2;
+    --res_mul2 <= std_logic_vector(mul_sg2);
     
 -- adder1
-    res_add_sg1 <= '0' & (mul_1 + mul_2); --Havia um 0 suspeito
+    res_add_sg1 <= '0' & (mul_sg1 + mul_sg2);
+    res_add1 <= std_logic_vector(res_add_sg1);
     
 -- adder2
-    res_add_sg2 <= '0' & (mul_3 + mul_4);
-    
--- adder3
-    res_add_sg3 <= '0' & (res_add_sg1 + res_add_sg2);
--- adder4
-    res_add_sg4 <= (res_add_sg3 + accum); 
+    --r1_sg <= signed(res_add_1);
+    reg1_sg <= signed(reg1);
+    res_add_sg2 <= '0' & (reg1_sg + res_add_sg1);
+    res_add2 <= std_logic_vector(res_add_sg2);
     
 -- register R1
 process (clk)
     begin
         if clk'event and clk='1' then
-            if rst_dpath ='1' then
-                reg_add_sg3 <= "0000000";
-                accum <= "0000000000000000";
-            else
-               if en_r1 = '1' then
-                    p_reg <= std_logic_vector(shift_right(unsigned(p_reg),4));
-                    w1_reg <= std_logic_vector(shift_right(unsigned(w1_reg),20));
-                    accum <=res_add_sg4;
-                end if;
-            end if;             
+            if reset ='1' then
+                reg1 <= "000000000000000000";
+            elsif en_r1 = '1' then
+                reg1 <= res_add1;
+            end if;
         end if;
     end process;
 
-res <= std_logic_vector(accum);
+-- register R2
+process (clk)
+    begin
+        if clk'event and clk='1' then
+            if reset ='1' then
+                reg2 <= "0000000000000000000";
+            elsif en_r2 = '1' then
+                reg2 <= res_add2;
+            end if;
+        end if;
+    end process;
+res <= reg2;
+feito <= done;
 end Behavioral;
